f093babdecff23a26e3965c021e11cfd77c6ef14
Isaac Canalizo <isaac.canamend@gmail.com>
2024-03-28 11:58:25 -0600

    Create array for validate all fields Set validations for delete and post method routes

--- routes/students.js
+++ routes/students.js
@@ -6,11 +6,19 @@ const { validarCampos } = require('../middlewares/validar-campos');
 
 const { studentsGet, 
         studentGet,
-        studentPatch, 
-        studentPut,
+        studentUpdate,
         studentPost, 
         studentDelete } = require('../controller/students');
         
+const dataValidator = [
+  body('nombre', 'El nombre es requerido').exists(),
+  body('nombre', 'Longitud inválida, el rango es de 10 a 70 caracteres')
+    .isLength({min: 10, max: 70}),
+  body('edad','La edad es requerida').exists(),
+  body('grupo','El grupo del alumno es requerido').exists(),
+  body('promedio_general','El promedio del alumno es requerido').exists(),
+  validarCampos
+];
 
 router.get('/', [], studentsGet);
 
@@ -19,21 +27,18 @@ router.get('/:id', [
   validarCampos
 ], studentGet);
 
-router.put('/:id', studentPut);
+router.put('/:id', [
+  check('id','no es un id válido').isNumeric(),
+  ...dataValidator,
+], studentUpdate);
 
 router.post('/', [
-  body('nombre', 'El nombre es requerido').exists(),
-  body('nombre', 'Longitud inválida, el rango es de 10 a 70 caracteres')
-    .isLength({min: 10, max: 70}),
-  body('edad','La edad es requerida').exists(),
-  body('grupo','El grupo del alumno es requerido').exists(),
-  body('promedio_general','El promedio del alumno es requerido').exists(),
-  validarCampos
+  ...dataValidator
 ], studentPost);
 
-router.delete('/:id', studentDelete);
-
-router.patch('/:id', studentPatch);
+router.delete('/:id', [
+  check('id','no es un id válido').isNumeric(),
+], studentDelete);
 
 
 module.exports = router;
